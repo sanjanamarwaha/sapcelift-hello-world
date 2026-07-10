49cd248b374cd0e4351e50a9506f33e101f7b3fd
Sanjana Marwaha <33581273+sanjanamarwaha@users.noreply.github.com>
2026-07-10 08:00:53 -0700

    Add Terraform configuration for random_pet resource

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+  }
+}
+ 
+resource "random_pet" "hello" {
+  length = 2
+}
+ 
+output "pet_name" {
+  value = random_pet.hello.id
+}
